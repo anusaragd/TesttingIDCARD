a1a14aa47a787141e5f2907033a400dc87017b68
MASTERS <anusara.orm@gmail.com>
2017-12-20 15:44:43 +0700

    change service

---
+++ app/src/main/java/com/example/masters/testtingidcard/webservice.java
@@ -0,0 +1,11 @@
+package com.example.masters.testtingidcard;
+
+/**
+ * Created by MASTERS on 20/12/2560.
+ */
+
+public class webservice {
+
+    String URL =  "http://10.0.0.43/webservice/WebService1.asmx";
+    String NAMESPACE = "http://tempuri.org/";
+}
