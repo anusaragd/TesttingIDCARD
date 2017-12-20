c07cfc9727cf73d9b13a627c570cd1d2c0a3593b
MASTERS <anusara.orm@gmail.com>
2017-12-20 15:26:45 +0700

    GGGGGG

--- app/src/main/java/com/example/masters/testtingidcard/CreateActivity.java
+++ app/src/main/java/com/example/masters/testtingidcard/CreateActivity.java
@@ -159,6 +159,9 @@ public class CreateActivity extends AppCompatActivity {
 //
 //    }
 
+    private String User = user.username;
+//    private String Room_name = nameroome.toString() ;
+
 
     public String UploadPictureViaWebservice() {
 
@@ -167,8 +170,8 @@ public class CreateActivity extends AppCompatActivity {
 
         String URL =  "http://10.0.0.43/webservice/WebService1.asmx";
         String NAMESPACE = "http://tempuri.org/";
-        String METHOD_NAME = "RegisterEnroll";
-        String SOAP_ACTION = "http://tempuri.org/RegisterEnroll/";
+        String METHOD_NAME = "Register_Room";
+        String SOAP_ACTION = "http://tempuri.org/Register_Room/";
         SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
 
         /**** with parameter *****/
@@ -176,148 +179,148 @@ public class CreateActivity extends AppCompatActivity {
 
 
         //CID
-        String strCardID = str_CID;
+        String strCardID = User;
         pi=new PropertyInfo();
-        pi.setName("ID_Card");
+        pi.setName("ID_Customer");
         pi.setValue(strCardID);
         pi.setType(String.class);
         request.addProperty(pi);
         //ShowMsg(strCardID);
 
         //ID_Room
-        String ID_Room = "123456";
+        String ID_Room = nameroome.toString();
         pi=new PropertyInfo();
-        pi.setName("ID_Room");
+        pi.setName("Description");
         pi.setValue(ID_Room);
         pi.setType(String.class);
         request.addProperty(pi);
         //ShowMsg(ID_Room);
 
-        //Name_Title_TH
-        String Name_Title_TH = varTitleTH;
-        pi=new PropertyInfo();
-        pi.setName("Name_Title_TH");
-        pi.setValue(Name_Title_TH);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(Name_Title_TH);
-
-        //Name_Title_EN
-        String Name_Title_EN = varTitleEN;
-        pi=new PropertyInfo();
-        pi.setName("Name_Title_EN");
-        pi.setValue(Name_Title_EN);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(Name_Title_EN);
-
-        //Name_TH
-        String Name_TH = varFNTH;
-        pi=new PropertyInfo();
-        pi.setName("Name_TH");
-        pi.setValue(Name_TH);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(Name_TH);
-
-        //LastName_TH  ****
-        String LastName_TH = varLNTH.replace(" ","").replace("\n","");
-        String LastName_TH_tmp = ConvStringToHex(LastName_TH);
-        LastName_TH_tmp = LastName_TH_tmp.substring(0, LastName_TH_tmp.indexOf("900"));
-        cnt = LastName_TH_tmp.length()/3;
-        LastName_TH = LastName_TH.substring(0,cnt);
-        pi=new PropertyInfo();
-        pi.setName("LastName_TH");
-        pi.setValue(LastName_TH);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(LastName_TH);
-
-        //Name_EN
-        String Name_EN = varFNEN;
-        pi=new PropertyInfo();
-        pi.setName("Name_EN");
-        pi.setValue(Name_EN);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(Name_EN);
-
-        //LastName_EN ****
-        String LastName_EN = varLNEN.replace(" ","").replace("\n","");
-        String LastName_EN_tmp = ConvStringToHex(LastName_EN);
-        LastName_EN_tmp = LastName_EN_tmp.substring(0, LastName_EN_tmp.indexOf("900"));
-        cnt = LastName_EN_tmp.length() / 2;
-        LastName_EN = LastName_EN.substring(0,cnt);
-        pi=new PropertyInfo();
-        pi.setName("LastName_EN");
-        pi.setValue(LastName_EN);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(LastName_EN);
-
-
-        if (varAddress==""){
-            //
-        }else{
-            varAddress = varAddress.replace("#"," ");
-            varAddress = varAddress.substring(0, varAddress.indexOf("@"));
-            varAddress = varAddress.replaceAll("\\s+", " ");
-            varAddress = varAddress.replace("\\0","").replace("\0","");
-        }
-        pi=new PropertyInfo();
-        pi.setName("Address");
-        pi.setValue(varAddress);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(varAddress);
-
-
-        //Gender
-        if (varGender=="ชาย"){
-            varGender = "0";
-        }else {
-            varGender = "1";
-        }
-        pi=new PropertyInfo();
-        pi.setName("Gender");
-        pi.setValue(varGender);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(varGender);
-
-
-        //Date_Of_Birth
-        pi=new PropertyInfo();
-        pi.setName("Date_Of_Birth");
-        pi.setValue(str_DOB);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(str_DOB);
-
-        //Date_Of_Issue
-        pi=new PropertyInfo();
-        pi.setName("Date_Of_Issue");
-        pi.setValue(strIssue);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(strIssue);
-
-        //Date_Of_Expiry
-        pi=new PropertyInfo();
-        pi.setName("Date_Of_Expiry");
-        pi.setValue(strExpire);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(strExpire);
-
-
-        //Imagebase64
-        pi=new PropertyInfo();
-        pi.setName("Imagebase64");
-        pi.setValue(imgBase64String);
-        pi.setType(String.class);
-        request.addProperty(pi);
-        //ShowMsg(imgBase64String);
+//        //Name_Title_TH
+//        String Name_Title_TH = varTitleTH;
+//        pi=new PropertyInfo();
+//        pi.setName("Name_Title_TH");
+//        pi.setValue(Name_Title_TH);
+//        pi.setType(String.class);
+//        request.addProperty(pi);
+//        //ShowMsg(Name_Title_TH);
+//
+//        //Name_Title_EN
+//        String Name_Title_EN = varTitleEN;
+//        pi=new PropertyInfo();
+//        pi.setName("Name_Title_EN");
+//        pi.setValue(Name_Title_EN);
+//        pi.setType(String.class);
+//        request.addProperty(pi);
+//        //ShowMsg(Name_Title_EN);
+//
+//        //Name_TH
+//        String Name_TH = varFNTH;
+//        pi=new PropertyInfo();
+//        pi.setName("Name_TH");
+//        pi.setValue(Name_TH);
+//        pi.setType(String.class);
+//        request.addProperty(pi);
+//        //ShowMsg(Name_TH);
+//
+//        //LastName_TH  ****
+//        String LastName_TH = varLNTH.replace(" ","").replace("\n","");
+//        String LastName_TH_tmp = ConvStringToHex(LastName_TH);
+//        LastName_TH_tmp = LastName_TH_tmp.substring(0, LastName_TH_tmp.indexOf("900"));
+//        cnt = LastName_TH_tmp.length()/3;
+//        LastName_TH = LastName_TH.substring(0,cnt);
+//        pi=new PropertyInfo();
+//        pi.setName("LastName_TH");
+//        pi.setValue(LastName_TH);
+//        pi.setType(String.class);
+//        request.addProperty(pi);
+//        //ShowMsg(LastName_TH);
+//
+//        //Name_EN
+//        String Name_EN = varFNEN;
+//        pi=new PropertyInfo();
+//        pi.setName("Name_EN");
+//        pi.setValue(Name_EN);
+//        pi.setType(String.class);
+//        request.addProperty(pi);
+//        //ShowMsg(Name_EN);
+//
+//        //LastName_EN ****
+//        String LastName_EN = varLNEN.replace(" ","").replace("\n","");
+//        String LastName_EN_tmp = ConvStringToHex(LastName_EN);
+//        LastName_EN_tmp = LastName_EN_tmp.substring(0, LastName_EN_tmp.indexOf("900"));
+//        cnt = LastName_EN_tmp.length() / 2;
+//        LastName_EN = LastName_EN.substring(0,cnt);
+//        pi=new PropertyInfo();
+//        pi.setName("LastName_EN");
+//        pi.setValue(LastName_EN);
+//        pi.setType(String.class);
+//        request.addProperty(pi);
+//        //ShowMsg(LastName_EN);
+//
+//
+//        if (varAddress==""){
+//            //
+//        }else{
+//            varAddress = varAddress.replace("#"," ");
+//            varAddress = varAddress.substring(0, varAddress.indexOf("@"));
+//            varAddress = varAddress.replaceAll("\\s+", " ");
+//            varAddress = varAddress.replace("\\0","").replace("\0","");
+//        }
+//        pi=new PropertyInfo();
+//        pi.setName("Address");
+//        pi.setValue(varAddress);
+//        pi.setType(String.class);
+//        request.addProperty(pi);
+//        //ShowMsg(varAddress);
+//
+//
+//        //Gender
+//        if (varGender=="ชาย"){
+//            varGender = "0";
+//        }else {
+//            varGender = "1";
+//        }
+//        pi=new PropertyInfo();
+//        pi.setName("Gender");
+//        pi.setValue(varGender);
+//        pi.setType(String.class);
+//        request.addProperty(pi);
+//        //ShowMsg(varGender);
+//
+//
+//        //Date_Of_Birth
+//        pi=new PropertyInfo();
+//        pi.setName("Date_Of_Birth");
+//        pi.setValue(str_DOB);
+//        pi.setType(String.class);
+//        request.addProperty(pi);
+//        //ShowMsg(str_DOB);
+//
+//        //Date_Of_Issue
+//        pi=new PropertyInfo();
+//        pi.setName("Date_Of_Issue");
+//        pi.setValue(strIssue);
+//        pi.setType(String.class);
+//        request.addProperty(pi);
+//        //ShowMsg(strIssue);
+//
+//        //Date_Of_Expiry
+//        pi=new PropertyInfo();
+//        pi.setName("Date_Of_Expiry");
+//        pi.setValue(strExpire);
+//        pi.setType(String.class);
+//        request.addProperty(pi);
+//        //ShowMsg(strExpire);
+//
+//
+////        //Imagebase64
+////        pi=new PropertyInfo();
+////        pi.setName("Imagebase64");
+////        pi.setValue(imgBase64String);
+////        pi.setType(String.class);
+////        request.addProperty(pi);
+////        //ShowMsg(imgBase64String);
 
         /*************************/
 
@@ -339,8 +342,8 @@ public class CreateActivity extends AppCompatActivity {
             strResponse = e.toString();
         }
 
-        logMsg(strResponse);
-        Toast.makeText(MainActivity.this,strResponse, Toast.LENGTH_SHORT).show();
+//        logMsg(strResponse);
+        Toast.makeText(CreateActivity.this,strResponse, Toast.LENGTH_SHORT).show();
 
         return strResponse;
 
--- app/src/main/java/com/example/masters/testtingidcard/LoginIDCardActivity.java
+++ app/src/main/java/com/example/masters/testtingidcard/LoginIDCardActivity.java
@@ -22,10 +22,15 @@ import static com.example.masters.testtingidcard.user.username;
 
 public class LoginIDCardActivity extends Activity {
 
+    EditText Username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_idcard);
+
+//        Username = (EditText)findViewById(R.id.username);
+//        Username.getText().toString();
 
     }
 
@@ -44,37 +49,37 @@ public class LoginIDCardActivity extends Activity {
 //                Intent i = new Intent(getApplicationContext(), Search.class);
 //                startActivity(i);
 
-                String NAMESPACE ="http://tempuri.org/";
-                String URL ="http://10.0.0.36/webservice/WebService1.asmx?";
-                String METHOD_NAME ="Login";
-                String SOAP_ACTION = "http://tempuri.org/Login";
-
-
-                SoapObject request = new SoapObject(NAMESPACE,METHOD_NAME);
-                request.addProperty("USR",Username.getText().toString());
-                request.addProperty("PWD",Password.getText().toString());
-
-
-                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-                envelope.dotNet = true;
-
-                envelope.setOutputSoapObject(request);
-
-                HttpTransportSE transportSE = new HttpTransportSE(URL);
-
-                try {
-                    transportSE.call(SOAP_ACTION,envelope);
-                    SoapPrimitive result = (SoapPrimitive) envelope.getResponse();
-                    res = result.toString();
-
-                } catch (IOException | XmlPullParserException e) {
-                    e.printStackTrace();
-                }
-
+//                String NAMESPACE ="http://tempuri.org/";
+//                String URL ="http://10.0.0.36/webservice/WebService1.asmx?";
+//                String METHOD_NAME ="Login";
+//                String SOAP_ACTION = "http://tempuri.org/Login";
+//
+//
+//                SoapObject request = new SoapObject(NAMESPACE,METHOD_NAME);
+//                request.addProperty("USR",Username.getText().toString());
+//                request.addProperty("PWD",Password.getText().toString());
+//
+//
+//                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+//                envelope.dotNet = true;
+//
+//                envelope.setOutputSoapObject(request);
+//
+//                HttpTransportSE transportSE = new HttpTransportSE(URL);
+//
+//                try {
+//                    transportSE.call(SOAP_ACTION,envelope);
+//                    SoapPrimitive result = (SoapPrimitive) envelope.getResponse();
+//                    res = result.toString();
+//
+//                } catch (IOException | XmlPullParserException e) {
+//                    e.printStackTrace();
+//                }
+//
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        username = Username.getText().toString(); ///// save Username
+                        username = Username.toString(); ///// save Username
                         Toast.makeText(LoginIDCardActivity.this,"aaaaaa",Toast.LENGTH_LONG).show();// แสดง Pop - up
                         Intent intent = new Intent(LoginIDCardActivity.this,CreateActivity.class);
                         startActivity(intent);
